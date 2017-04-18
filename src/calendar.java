5c58534acfc2315b5a24a62bb07909a28426b9aa
Exposed <exposedgun316@live.co.uk>
2017-04-18 09:45:54 +0100

    Adding in packages

--- src/calendar.java
+++ src/calendar.java
@@ -1,6 +1,29 @@
 /**
  * Created by Ronan on 18/04/2017.
  */
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 public class calendar {
-test
-}
+
+    public class calendarProgram{
+        static JLabel lblMonth, lblYear;
+        static JButton btnPrev, btnNext;
+        static JTable tblCalendar;
+        static JComboBox cmbYear;
+        static JFrame frmMain;
+        static Container pane;
+        static DefaultTableModel mtblCalendar; //Table model
+        static JScrollPane stblCalendar; //The scrollpane
+        static JPanel pnlCalendar; //The panel
+        static int realDay, realMonth, realYear, currentMonth, currentYear;
+
+        public static void main (String args[]) {
+
+            }
+        }
+    }
